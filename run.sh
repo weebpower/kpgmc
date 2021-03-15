670256a766b51e7db78f2cbe02ae5690f22ebd1a
edeak <nukesurge@gmail.com>
2021-03-15 04:43:34 +0000

    manual backup

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-trap exit INT
+
 while :
 do
-	rlwrap -a java -Xmx2048M -Xms2048M -jar server.jar nogui
+	# Loop continues as long as starting the server fails
+	rlwrap -a java -Xmx2048M -Xms2048M -jar spigot.jar nogui && break
 done
